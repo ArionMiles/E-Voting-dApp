44247d53fc5a3fb55a38b10e3fb0c7d947ca0175
Kanishk Singh <singh.kanishk07@gmail.com>
2018-09-22 09:23:05 +0530

    added referendum and fixed pie chart

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":true,"inputs":[],"name":"chairperson","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"voter","type":"address"}],"name":"giveRightToVote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"voters","outputs":[{"name":"voted","type":"bool"},{"name":"weight","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0xa6af0b0a7605a588f1e82cb7728ca71a42517985');
+contractInstance = VotingContract.at('0x5ab6a492aa585ec2ee787d2ea47c50bb8776af1f');
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
 
 function voteForCandidate(candidate) {
   candidateName = $("#candidate").val();
   voterToken = $("#token").val();
   try {
-    contractInstance.voteForCandidate(candidateName, {from: getTokenVal()}, function() {
+    contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[1]}, function() {
       let div_id = candidates[candidateName];
       $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
 
     });
   } catch (err) {
+    console.log(err);
   }
 }
 
@@ -27,11 +28,17 @@ $(document).ready(function() {
     $("#" + candidates[name]).html(val);
     array_of_votes.push(val)
   var data = [{
-    x: candidateNames,
-    y: array_of_votes,
-    type: 'bar'
+    labels: candidateNames,
+    values: array_of_votes,
+    type: 'pie'
   }];
-  Plotly.newPlot('votingGraph', data);
+  
+  var layout = {
+    height: 400,
+    width: 500
+  };
+
+  Plotly.newPlot('votingGraph', data, layout);
   //plotVotingGraph(candidateNames, array_of_votes)
   }
 });
@@ -56,9 +63,8 @@ function plotVotingGraph() {
   Plotly.newPlot('votingGraph', data, layout);
 }}
 
-function setTokenCookie() {
+function setTokenCookie(tokenVal) {
   // Set Token Cookie
-  var tokenVal = $("#token").value
   document.cookie = `token=${tokenVal}; path=/`
   document.getElementById("index_block").hidden = false;
   document.getElementById("login").hidden = true;
